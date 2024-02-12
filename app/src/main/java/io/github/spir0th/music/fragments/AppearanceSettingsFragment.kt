a9b63dd5fb38d6dd77976ba55e67a3f2b81ba9c8
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2024-02-13 03:14:26 +0800

    feat: Toggling dynamic colors will now display an option to restart

--- app/src/main/java/io/github/spir0th/music/fragments/AppearanceSettingsFragment.kt
+++ app/src/main/java/io/github/spir0th/music/fragments/AppearanceSettingsFragment.kt
@@ -1,9 +1,12 @@
 package io.github.spir0th.music.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.jakewharton.processphoenix.ProcessPhoenix
 import io.github.spir0th.music.R
 import io.github.spir0th.music.activities.SettingsActivity
 
@@ -13,7 +16,22 @@ class AppearanceSettingsFragment : PreferenceFragmentCompat() {
         val dynamicColors = findPreference<SwitchPreferenceCompat>("dynamic_colors")
 
         dynamicColors?.setOnPreferenceChangeListener { _, _ ->
-            Toast.makeText(requireContext(), R.string.settings_restart, Toast.LENGTH_LONG).show()
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.dialog_dynamic_colors_restart_title)
+                .setMessage(R.string.dialog_dynamic_colors_restart_message)
+                .setPositiveButton(R.string.dialog_dynamic_colors_restart_positive) { dialog, _ ->
+                    dialog.dismiss() // Dismiss dialog
+
+                    // Restart application
+                    val intent = Intent(requireContext(), SettingsActivity::class.java)
+                    ProcessPhoenix.triggerRebirth(requireContext(), intent)
+                }
+                .setNegativeButton(R.string.dialog_dynamic_colors_restart_negative) { dialog, _ ->
+                    dialog.dismiss() // Only dismiss dialog
+                }
+                .setCancelable(false)
+                .show()
+
             true
         }
     }
