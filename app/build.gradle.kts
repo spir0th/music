94af77a91da10d74c0d61fa87aec23e75006ab2b
spir0th <spir.0th.xyz@gmail.com>
2024-02-12 17:53:28 +0800

    build: Remove unnecessary androidx.palette dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
     implementation("androidx.browser:browser:1.7.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
