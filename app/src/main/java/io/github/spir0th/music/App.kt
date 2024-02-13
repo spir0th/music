e9f75ca41eb2c537a269afbf8bb068881c32407e
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2024-02-13 15:21:32 +0800

    feat: Generate a crash log when a fatal exception is thrown

--- app/src/main/java/io/github/spir0th/music/App.kt
+++ app/src/main/java/io/github/spir0th/music/App.kt
@@ -5,18 +5,41 @@ import android.content.SharedPreferences
 import android.util.Log
 import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.system.exitProcess
 
-class App : Application() {
+class App : Application(), Thread.UncaughtExceptionHandler {
     private lateinit var preferences: SharedPreferences
-
     override fun onCreate() {
         super.onCreate()
         preferences = PreferenceManager.getDefaultSharedPreferences(this)
-        Log.i(TAG, "${getString(R.string.app_name)} ${BuildConfig.VERSION_CODE}")
+        Thread.setDefaultUncaughtExceptionHandler(this)
+        Log.i(TAG, "Initialized ${getString(R.string.app_name)} version ${BuildConfig.VERSION_NAME} (code ${BuildConfig.VERSION_CODE})")
 
         if (preferences.getBoolean("dynamic_colors", true)) {
             DynamicColors.applyToActivitiesIfAvailable(this)
         }
+    }
+
+    override fun uncaughtException(t: Thread, e: Throwable) {
+        Log.e(TAG, "A fatal exception has occurred in the application, generating crash log.")
+        val dateTime = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val logDir = File(dataDir, "logs")
+        val file = File(logDir, "crash-$dateTime.log")
+
+        apply {
+            logDir.mkdirs()
+            file.createNewFile()
+        }
+        file.printWriter().use { out ->
+            out.println(e.stackTraceToString())
+        }
+
+        Log.i(TAG, "A crash log has been generated, now exiting!")
+        exitProcess(-1)
     }
 
     companion object {
