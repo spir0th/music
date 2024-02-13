5136ca0e1a73905fa9a22073ca2a98f988352b43
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2024-02-13 14:35:58 +0800

    feat: Remove playback resumption because it is unnecessary

--- app/src/main/java/io/github/spir0th/music/services/PlaybackService.kt
+++ app/src/main/java/io/github/spir0th/music/services/PlaybackService.kt
@@ -4,12 +4,10 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.media3.common.AudioAttributes
-import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 import androidx.preference.PreferenceManager
-import com.google.common.util.concurrent.ListenableFuture
 import io.github.spir0th.music.activities.MusicActivity
 
 class PlaybackService : MediaSessionService() {
@@ -23,14 +21,6 @@ class PlaybackService : MediaSessionService() {
             .setAudioAttributes(AudioAttributes.DEFAULT, preferences.getBoolean("audio_focus", true))
             .build()
         mediaSession = MediaSession.Builder(this, player)
-            .setCallback(@UnstableApi object: MediaSession.Callback {
-                override fun onPlaybackResumption(
-                    mediaSession: MediaSession,
-                    controller: MediaSession.ControllerInfo
-                ): ListenableFuture<MediaSession.MediaItemsWithStartPosition> {
-                    return super.onPlaybackResumption(mediaSession, controller)
-                }
-            })
             .setSessionActivity(PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE))
             .build()
     }
