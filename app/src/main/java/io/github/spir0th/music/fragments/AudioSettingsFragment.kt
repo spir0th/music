2770bfc702fdd7b8f29f24c4559628a12ab05dd3
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2024-02-15 17:44:38 +0800

    feat: Actually toggle audio focus at runtime instead of requiring a restart

--- app/src/main/java/io/github/spir0th/music/fragments/AudioSettingsFragment.kt
+++ app/src/main/java/io/github/spir0th/music/fragments/AudioSettingsFragment.kt
@@ -1,34 +1,33 @@
 package io.github.spir0th.music.fragments
 
+import android.content.ComponentName
 import android.os.Bundle
 import android.text.InputType
+import androidx.media3.common.AudioAttributes
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import androidx.preference.CheckBoxPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.common.util.concurrent.MoreExecutors
 import io.github.spir0th.music.R
 import io.github.spir0th.music.activities.SettingsActivity
+import io.github.spir0th.music.services.PlaybackService
 import io.github.spir0th.music.utils.cleanMediaPersists
 
 class AudioSettingsFragment : PreferenceFragmentCompat() {
+    private var mediaController: MediaController? = null
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences_audio, rootKey)
         val audioFocus = findPreference<CheckBoxPreference>("audio_focus")
         val timeGetDuration = findPreference<EditTextPreference>("time_get_duration")
         val cleanPersistence = findPreference<Preference>("clean_persistence")
 
-        audioFocus?.setOnPreferenceChangeListener { _, _ ->
-            MaterialAlertDialogBuilder(requireContext())
-                .setTitle(R.string.dialog_audio_focus_restart_title)
-                .setMessage(R.string.dialog_audio_focus_restart_message)
-                .setPositiveButton(R.string.dialog_audio_focus_restart_positive) { _, _ ->
-                    (requireActivity() as SettingsActivity).restartApplication() // Restart application
-                }
-                .setNegativeButton(R.string.dialog_audio_focus_restart_negative) { _, _ -> }
-                .setCancelable(false)
-                .show()
-
+        audioFocus?.setOnPreferenceChangeListener { _, newValue ->
+            mediaController?.setAudioAttributes(AudioAttributes.DEFAULT, newValue as Boolean)
             true
         }
         timeGetDuration?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
@@ -77,5 +76,17 @@ class AudioSettingsFragment : PreferenceFragmentCompat() {
             val activity = requireActivity() as SettingsActivity
             activity.supportActionBar?.title = getString(R.string.prefs_audio)
         }
+
+        val sessionToken = SessionToken(requireContext(), ComponentName(requireContext(), PlaybackService::class.java))
+        val controllerFuture = MediaController.Builder(requireContext(), sessionToken).buildAsync()
+
+        controllerFuture.addListener({
+            mediaController = controllerFuture.get()
+        }, MoreExecutors.directExecutor())
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaController?.release()
     }
 }
