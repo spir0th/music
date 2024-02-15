3c2d719d4250404c3cd3f45cb748136ea373942d
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2024-02-15 18:03:05 +0800

    build[app]: Remove unused Media3 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.jakewharton:process-phoenix:2.1.2")
     implementation("androidx.media3:media3-exoplayer:1.2.1")
-    implementation("androidx.media3:media3-ui:1.2.1")
     implementation("androidx.media3:media3-common:1.2.1")
     implementation("androidx.media3:media3-session:1.2.1")
     implementation("androidx.browser:browser:1.7.0")
